fc679fc624d0cad2e1476907da113f1448a0aa8d
nakasyou <syoutarou1103@outlook.jp>
2023-06-24 05:50:50 +0000

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'npm:vite'
-import * as path from "https://deno.land/std@0.192.0/path/mod.ts";
+import { defineConfig } from 'vite'
+import path from "node:path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve('./src/index.ts'),
+      entry: path.resolve(__dirname, './src/index.ts'),
       name: 'nueos',
       fileName: 'index',
       formats: ['es','cjs','umd','iife'],
     },
     target: 'esnext',
-    outDir: path.resolve('./dist'),
+    outDir: path.resolve(__dirname, './dist'),
     rollupOptions: {
       external: [],
       output: {
